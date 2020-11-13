dfe389de5c4d1e9b5e0c0f83d1c414d6349b0149
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2020-11-12 20:57:14 -0500

    initial commit

---
+++ src/SortingShenanigans.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class SortingShenanigans {
+
+    public static void main(String[] args) {
+        //UI GOES HERE
+    }
+
+    private static ArrayList<Integer> createUnsortedArray(int arraySize, int lowerBound, int upperBound) {
+        //CREATE THE UNSORTED ARRAYS FOR TESTS
+        return null;
+    }
+
+    public static void complexityAnalysis(String sortChoice, int arraySize, int testRuns) {
+        //TEST RUNS HERE
+    }
+
+    public static boolean bogoSort(ArrayList<Integer> sortMe) {
+        return false;
+    }
+
+    public static boolean bubbleSort(ArrayList<Integer> sortMe) {
+        return false;
+    }
+
+    public static boolean selectionSort(ArrayList<Integer> sortMe) {
+        return false;
+    }
+
+    public static boolean insertionSort(ArrayList<Integer> sortMe) {
+        return false;
+    }
+
+    public static boolean quickSort(ArrayList<Integer> sortMe) {
+        return false;
+    }
+
+    public static boolean mergeSort(ArrayList<Integer> sortMe) {
+        return false;
+    }
+
+    public static boolean heapSort(ArrayList<Integer> sortMe) {
+        return false;
+    }
+
+    private static void isSorted(ArrayList<Integer> testMe) throws Exception{
+        //VALIDATE IF ARRAYLIST IS SORTED
+        for(int i = 1; i < testMe.size(); i++) {
+            if(testMe.get(i-1) > testMe.get(i)) {
+                throw new Exception("ArrayList unsorted, index i-1 > index i : " + testMe.get(i-1) + " > " + testMe.get(i));
+            }
+        }
+    }
+
+    // Modified Version
+
+    private static int isUnsorted(ArrayList<Integer> testMe){
+        //VALIDATE IF ARRAYLIST IS SORTED
+        // Returns index of unsorted pair
+        // If sorted, returns -1
+        for(int i = 1; i < testMe.size(); i++) {
+            if(testMe.get(i-1) > testMe.get(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
