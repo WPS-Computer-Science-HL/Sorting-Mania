43c2c81146c2e8be84082974362d794f59b53710
SamLedden <44735694+pixlr20@users.noreply.github.com>
2020-11-17 08:57:43 -0500

    Update SortingShenanigans.java

--- src/SortingShenanigans.java
+++ src/SortingShenanigans.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 public class SortingShenanigans {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //UI GOES HERE
 
         // TEST A change made
+        Random rand = new Random();
+        ArrayList<Integer> test = new ArrayList<Integer>();
+        for(int i = 0; i < 10; i++) {
+            test.add(rand.nextInt(10));
+        }
+        heapSort(test);
     }
 
     // TEST -b change made here
@@ -44,42 +51,42 @@ public class SortingShenanigans {
         return false;
     }
 
-    public static void heapify(ArrayList<Integer> heap, int index, int heapCap) {
+    private static void swap(ArrayList<Integer> swapMe, int indexA, int indexB){
+        int temp = swapMe.get(indexA);
+        swapMe.set(indexA, swapMe.get(indexB));
+        swapMe.set(indexB, temp);
+    }
+
+    private static void heapify(ArrayList<Integer> heap, int index, int heapCap) {
         int leftChildIndex = index * 2 + 1;
         int rightChildIndex = index * 2 + 2;
+        int largest = index;
+        
+        if(leftChildIndex < heapCap && heap.get(leftChildIndex) > heap.get(largest)) {
+            largest = leftChildIndex;
+        } 
 
-        if(index >= heapCap) {
-            return;
-        } else if(heap.size() <= leftChildIndex) {
-            return;
-        } else if(heap.size() <= rightChildIndex) {
-            if(heap.get(leftChildIndex) > heap.get(index)) {
-                int temp = heap.get(leftChildIndex);
-                heap.set(leftChildIndex, heap.get(index));
-                heap.set(index, temp);
-            }
-            return;
-        } else {
-            if(heap.get(index) < heap.get(leftChildIndex) && heap.get(leftChildIndex) > heap.get(rightChildIndex)) {
-                int temp = heap.get(leftChildIndex);
-                heap.set(leftChildIndex, heap.get(index));
-                heap.set(index, temp);
-            } else if(heap.get(index) < heap.get(rightChildIndex)) {
-                int temp = heap.get(rightChildIndex);
-                heap.set(rightChildIndex, heap.get(index));
-                heap.set(index, temp);
-            }
-            heapify(heap, leftChildIndex, heapCap);
-            heapify(heap, rightChildIndex, heapCap);
+        if(rightChildIndex < heapCap && heap.get(rightChildIndex) > heap.get(largest)) {
+            largest = rightChildIndex;
+        } 
+
+        if(largest != index) {
+            swap(heap, largest, index);
+        }
+    }
+
+    private static void heapify(ArrayList<Integer> heap, int heapCap) {
+        int start = (heapCap / 2) - 1;
+
+        for(int i = start; i >= 0; i--) {
+            heapify(heap, i, heapCap);
         }
     }
 
     public static boolean heapSort(ArrayList<Integer> heap) throws Exception{
-        heapify(heap, 0, heap.size());
+        heapify(heap, heap.size());
         for(int i = 0; i < heap.size(); i++) {
-            int temp = heap.get(0);
-            heap.set(0, heap.get(heap.size() - i - 1));
-            heap.set(heap.get(heap.size() - i - 1), temp);
+            swap(heap, 0, heap.size() - i - 1);
             heapify(heap, 0, heap.size() - i - 1);
         }
         isSorted(heap);
