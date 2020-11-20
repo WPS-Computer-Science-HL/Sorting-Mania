30104f0860c30d420abd760714ac4a53e88ba44d
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2020-11-20 15:18:24 -0500

    minor changes

--- src/SortingShenanigans.java
+++ src/SortingShenanigans.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 public class SortingShenanigans {
 
+    // Pretty colors :)
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
